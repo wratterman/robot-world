a7509d8b01cee286c36f38018563943bd8d2e969
wratterman <will.ratterman@gmail.com>
2017-06-27 14:22:16 -0600

    initial commit, added dashboard link

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,11 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :dashboard
+  end
+
   get '/robots' do
     erb :index
   end
 end
-
